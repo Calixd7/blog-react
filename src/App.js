f233db4502a8eb59e2793ca702eb1b8a09b686e5
David Calix <calixd7@gmail.com>
2021-09-23 00:52:09 -0400

    added background image to app bar styles

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { AppBar } from '@mui/material';
 import { Toolbar } from '@mui/material';
 import { Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
-import { Box } from '@mui/material/Box';
+import Box from '@mui/material/Box';
 
 const useStyles = makeStyles({
-  AppBar: {
+  appBar: {
     backgroundColor: "#fff"
   
   },
   hero: {
-    backgroundImage: 'linear-gradient(rgba(0,0,0,0.5)'
+    backgroundImage:  `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
   }
 });
 
@@ -21,14 +21,18 @@ function App() {
 
   return (
     <div className="App">
-       <AppBar className={classes.AppBar} position="static" color="default">
+       <AppBar className={classes.appBar} position="static" color="default">
         <Toolbar>
           <Typography variant="h6" color='primary' >
             Blog
           </Typography>
         </Toolbar>
       </AppBar>
-    </div>
+     <Box className={classes.hero}>
+        <Box>React Blog</Box>
+    </Box> 
+  </div>
+
   );
 }
 
