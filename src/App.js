aae2cd33838c3a1febe37882ea4fed4d5d967910
David Calix <calixd7@gmail.com>
2021-09-22 21:44:39 -0400

    added appbar and theme with Material UI library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar } from '@mui/material';
+import { Toolbar } from '@mui/material';
+import { Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Box } from '@mui/material/Box';
+
+const useStyles = makeStyles({
+  AppBar: {
+    backgroundColor: "#fff"
+  
+  },
+  hero: {
+    backgroundImage:  'linear-gradient(rgba(0,0,0,0.5)'
+  }
+});
+
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AppBar className={classes.AppBar} position="static">
+        <Toolbar>
+          <Typography variant="h6" color='primary' >
+            Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme ({
+    palette: {
+        primary: {
+            main: "#764abc" 
+        },
+    },
+     background: {
+         default: "#fff"
+     }
+})
+
+export default theme;
