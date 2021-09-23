dfe4d5a139bc2f51f190f60419d17547443dfd70
David Calix <calixd7@gmail.com>
2021-09-23 01:02:33 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import { Toolbar } from '@mui/material';
 import { Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import Box from '@mui/material/Box';
+import { theme } from './theme';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   appBar: {
     backgroundColor: "#fff"
   
   },
   hero: {
     backgroundImage:  `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
-  }
-});
-
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+    }
+  }));
 
 function App() {
   const classes = useStyles();
